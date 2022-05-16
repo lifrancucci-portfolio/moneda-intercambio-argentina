51765897f124d359ab904471639ea6685b4b8070
Lisandro Francucci <lisandrofrancucci@tutanota.com>
2022-05-16 13:50:00 -0300

    Disable scroll when Menu is displaying

--- js/main.js
+++ js/main.js
@@ -1,21 +1,43 @@
 function contentLoaded() {
 
   // MENU
+  const mainContainer = document.getElementById('main-container');
   const header = document.getElementById('header');
   const hamburguerMenu = document.getElementById('hamburguer-menu');
   const navMenu = document.getElementById('nav-menu');
   const closeMenuButton = document.getElementById('close-menu-button');
   const menuLinks = document.querySelectorAll('.menu-link')
 
+  // Evitar scroll cuando el Menú está desplegado
+  function preventScroll(e) {
+    console.log("Calling the function");
+    e.preventDefault();
+    e.stopPropagation();
+    return false;
+  }
+  function disableScroll() {
+    mainContainer.addEventListener('wheel', preventScroll);
+  }
+  function enableScroll() {
+    mainContainer.removeEventListener('wheel', preventScroll);
+  }
+
+  // Desplegar Menú
   function openMenu() {
     header.classList.add('menu-expanded');
     navMenu.style.display = 'flex';
+    // Deshabilitar Scroll
+    disableScroll();
   }
 
+  // Cerrar Menú
   function closeMenu() {
     header.classList.remove('menu-expanded');
     navMenu.style.display = 'none';
+    // Rehabilitar Scroll
+    enableScroll();
   }
+
 
 
   hamburguerMenu.addEventListener('click', openMenu);
@@ -23,8 +45,5 @@ function contentLoaded() {
   menuLinks.forEach(menuLink => {
     menuLink.addEventListener('click', closeMenu);
   });
-
-
-
 
 }
