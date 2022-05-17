2a774207fbf3fea4abc0b0768974b5824ae18764
Lisandro Francucci <lisandrofrancucci@tutanota.com>
2022-05-17 12:24:07 -0300

    Refactoring for slides selection and timer

--- js/main.js
+++ js/main.js
@@ -55,15 +55,17 @@ function contentLoaded() {
   let currentSlide = 0;
 
   // Cambiar slide cada n milisegundos
-  setInterval(function() {selectSlide(currentSlide)}, 2500);
+  let slidesTimer = setInterval(function() {selectSlide(currentSlide)}, 2500);
 
   function selectSlide(target) {
     // Cambiar a la slide n mediante los selectores
     if(currentSlide < heroTextSlides.length) {
       heroTextSlides.forEach(slide => {
-        slide.style.display = "none";
+        // slide.style.opacity = 0;
+        slide.style.display = 'none';
       })
       heroTextSlides[target].style.display = 'block';
+      // heroTextSlides[target].style.opacity = 1;
 
       // Indicar qué selector está seleccionado
       carrouselSelectors.forEach(selector => {
@@ -82,27 +84,29 @@ function contentLoaded() {
   carrouselSelectors.forEach(carrouselSelector => {
     carrouselSelector.addEventListener('click', function handleClick(event) {
       let e = event.target.classList;
-      // console.log(e);
       if(e.contains('selector1')) {
-        selectSlide(0);
         currentSlide = 0;
       }
       if(e.contains('selector2')) {
-        selectSlide(1);
         currentSlide = 1;
       }
       if(e.contains('selector3')) {
-        selectSlide(2);
         currentSlide = 2;
       }
+      // Seleccionar el slide elegido
+      selectSlide(currentSlide);
+
+      // Resetear slidesTimer
+      window.clearInterval(slidesTimer);
+      slidesTimer = window.setInterval(function() {selectSlide(currentSlide)}, 2500);
     });
   })
 
   // PREGUNTAS FRECUENTES ACCORDION
-  const questionDisplay = document.getElementsByClassName('question-display');
+  const questionDisplay = document.querySelectorAll('.question-display');
 
-  for(let i = 0; i < questionDisplay.length; i++) {
-    questionDisplay[i].addEventListener('click', function() {
+  questionDisplay.forEach(question => {
+    question.addEventListener('click', function() {
       this.classList.toggle('active');
       let expandText = this.nextElementSibling;
       if(expandText.style.maxHeight) {
@@ -110,7 +114,18 @@ function contentLoaded() {
       } else {
         expandText.style.maxHeight = expandText.scrollHeight + 'px';
       }
-    });
-  }
+    })
+  })
+  // for(let i = 0; i < questionDisplay.length; i++) {
+  //   questionDisplay[i].addEventListener('click', function() {
+  //     this.classList.toggle('active');
+  //     let expandText = this.nextElementSibling;
+  //     if(expandText.style.maxHeight) {
+  //       expandText.style.maxHeight = null;
+  //     } else {
+  //       expandText.style.maxHeight = expandText.scrollHeight + 'px';
+  //     }
+  //   });
+  // }
 
 }
